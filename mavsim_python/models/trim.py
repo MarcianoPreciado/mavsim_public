389a184fb9413424758cb6df995d72ba0c0bc228
Marciano C. Preciado <marcianocpreciado@gmail.com>
2026-05-27 21:26:30 -0600

    feat: implement trim cost-function

--- mavsim_python/models/trim.py
+++ mavsim_python/models/trim.py
@@ -6,19 +6,44 @@ compute_trim
 """
 import numpy as np
 from scipy.optimize import minimize
-from tools.rotations import euler_to_quaternion
+from parameters.aerosonde_parameters import (
+    # physical properties
+    gravity, mass, rho, Jy,
+    # geometry
+    S_wing, b, c, AR, e,
+    # motor / propeller
+    V_max, KV, KQ, R_motor, i0,
+    D_prop, C_Q0, C_Q1, C_Q2, C_T0, C_T1, C_T2,
+    # stall model
+    M, alpha0,
+    # stability derivatives
+    gamma1, gamma2, gamma3, gamma4, gamma5, gamma6, gamma7,
+    # lift and drag
+    C_L_0, C_L_alpha, C_L_q, C_L_delta_e,
+    C_D_0, C_D_alpha, C_D_p, C_D_q, C_D_delta_e,
+    # side force
+    C_Y_0, C_Y_beta, C_Y_p, C_Y_r, C_Y_delta_a, C_Y_delta_r,
+    # pitch moment
+    C_m_0, C_m_alpha, C_m_q, C_m_delta_e,
+    # roll moment
+    C_p_0, C_p_beta, C_p_p, C_p_r, C_p_delta_a, C_p_delta_r,
+    # yaw moment
+    C_r_0, C_r_beta, C_r_p, C_r_r, C_r_delta_a, C_r_delta_r,
+)
+from tools.rotations import euler_to_quaternion, quaternion_to_euler
 from message_types.msg_delta import MsgDelta
 import time
+from math import sin, cos, tan
 
 def compute_trim(mav, Va, gamma):
     # define initial state and input
 
     ##### TODO #####
     # set the initial conditions of the optimization
-    state0 = np.array([[0],  # pn
-                   [0],  # pe
-                   [0],  # pd
     e0 = euler_to_quaternion(0., gamma, 0.)
+    state0 = np.array([[5],  # pn
+                   [5],  # pe
+                   [-Va * sin(gamma)],  # pd
                    [0],  # u
                    [0.], # v
                    [0.], # w
@@ -34,7 +59,7 @@ def compute_trim(mav, Va, gamma):
                        [0],  # aileron
                        [0],  # rudder
                        [0]]) # throttle
-    x0 = np.concatenate((state0, delta0), axis=0)
+    x0 = np.concatenate((state0, delta0), axis=0).flatten()
     # define equality constraints
     cons = ({'type': 'eq',
              'fun': lambda x: np.array([
@@ -74,8 +99,82 @@ def compute_trim(mav, Va, gamma):
     return trim_state, trim_input
 
 
+def motor_thrust_torque(Va, delta_t):
+        # compute thrust and torque due to propeller
+        # map delta_t throttle command(0 to 1) into motor input voltage
+        Vin = V_max * delta_t
+        a = rho * D_prop**5 / (2 * pi)**2 * C_Q0
+        b = rho * D_prop**4 / (2 * pi) * C_Q1 * Va + KV * KQ/R_motor
+        c = rho * D_prop**3 * C_Q2 * Va**2 - KQ / R_motor * Vin + KQ * i0
+        roots = np.roots([a, b, c])
+        # Angular speed
+        omega_p = max(roots[roots >= 0])
+
+        # thrust and torque due to propeller
+        torque_prop = (rho * D_prop**5 * C_Q0/4/pi**2) * omega_p**2 + (rho * D_prop**4 * C_Q1 * Va /2 /pi) * omega_p + rho * D_prop**3 * C_Q2 * Va**2
+        thrust_prop = (rho * D_prop**4 * C_T0 / 4 / pi**2) * omega_p**2 + (rho * D_prop**3 * C_T1 * Va / 2 / pi) * omega_p + rho * D_prop**2 * C_T2 * Va**2
+
+        return thrust_prop, torque_prop
+
+
 def trim_objective_fun(x, mav, Va, gamma):
     # objective function to be minimized
-    ##### TODO #####
-    J = 0
+    pn = x.item(0)
+    pe = x.item(1)
+    pd = x.item(2)
+    u = x.item(3)
+    v = x.item(4)
+    w = x.item(5)
+    e0 = x.item(6)
+    e1 = x.item(7)
+    e2 = x.item(8)
+    e3 = x.item(9)
+    p = x.item(10)
+    q = x.item(11)
+    r = x.item(12)
+    delta_elevator = x.item(13)
+    delta_aileron = x.item(14)
+    delta_rudder = x.item(15)
+    delta_throttle = x.item(16)
+
+    phi, theta, psi = quaternion_to_euler(np.array([[e0], [e1], [e2], [e3]]))
+    # Compute the helpful variables for state derivatives
+    # True forms
+    # sigmoid = (1 + exp(-M * (self._alpha - alpha0)) + exp(M * (self._alpha + alpha0))) / ((1 + exp(-M * (self._alpha - alpha0))) * (1 + exp(M * (self._alpha + alpha0))))
+    # CL = (1 - sigmoid) * (C_L_0 + C_L_alpha * self._alpha) + sigmoid * 2 * np.sign(self._alpha) * (sin(self._alpha))**2 * cos(self._alpha)
+    # CD = C_D_p + (C_L_0 + C_L_alpha * self._alpha)**2 / (pi * e * AR)
+    # TODO do we assume alpha is zero? ^^^
+
+    h = -pd
+    alpha = 0
+    beta = 0
+
+    CL = C_L_0 + C_L_alpha * alpha
+    CD = C_D_0 + C_D_alpha * alpha
+
+    CX = -CD * cos(alpha) + CL * sin(alpha)
+    CXq = -C_D_q * cos(alpha) + C_L_q * sin(alpha)
+    CXdelta_e = -C_D_delta_e * cos(alpha) + C_L_delta_e * sin(alpha)
+    CZ = -CD * sin(alpha) - CL * cos(alpha)
+    CZq = -C_D_q * sin(alpha) - C_L_q * cos(alpha)
+    CZdelta_e = -C_D_delta_e * sin(alpha) - C_L_delta_e * cos(alpha)
+
+    # compute the state derivatives using the nonlinear equations of motion
+    # ignore pn, pe, pd since they are intended to be at steady rates of change
+    Tp, Qp = mav._motor_thrust_torque(Va, delta_throttle)
+    
+    udot = r*v - q*w - gravity * sin(theta) + rho * Va**2 * S_wing / (2 * mass) * (CX + CXq * c*q/(2*Va) + CXdelta_e * delta_elevator) + Tp/mass
+    vdot = p*w - r*u + gravity * cos(theta) * sin(phi) + rho * Va**2 * S_wing / (2 * mass) * (C_Y_0 + C_Y_beta * 0 + C_Y_p * (b / (2 * Va)) * p + C_Y_r * (b / (2 * Va)) * r + C_Y_delta_a * delta_aileron + C_Y_delta_r * delta_rudder)
+    wdot = q*u - p*v + gravity * cos(theta) * cos(phi) + rho * Va**2 * S_wing / (2 * mass) * (CZ + CZq * c*q/(2*Va) + CZdelta_e * delta_elevator)
+
+    phidot = p + q*sin(phi)*tan(theta) + r*cos(phi)*tan(theta)
+    thetadot = q*cos(phi) - r*sin(phi)
+    psidot = q*sin(phi)/cos(theta) + r*cos(phi)/cos(theta)
+
+    pdot = gamma1*p*q - gamma2*q*r + 0.5*rho*Va**2*S_wing*b*(C_p_0 + C_p_beta * beta + C_p_p * (b / (2 * Va)) * p + C_p_r * (b / (2 * Va)) * r + C_p_delta_a * delta_aileron + C_p_delta_r * delta_rudder) + gamma3*Qp
+    qdot = gamma5*p*r - gamma6*(p**2 - r**2) + rho * Va**2 *S_wing*c/2/Jy *(C_m_0 + C_m_alpha * alpha + C_m_q * (c / (2 * Va)) * q + C_m_delta_e * delta_elevator)
+    rdot = gamma7*p*q - gamma1*q*r + 0.5*rho*Va**2*S_wing*b*(C_r_0 + C_r_beta * beta + C_r_p * (b / (2 * Va)) * p + C_r_r * (b / (2 * Va)) * r + C_r_delta_a * delta_aileron + C_r_delta_r * delta_rudder) + gamma4*Qp
+
+    
+    J = vdot**2 + wdot**2 + phidot**2 + thetadot**2 + psidot**2 + pdot**2 + qdot**2 + rdot**2
     return J
