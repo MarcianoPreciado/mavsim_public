c959edc68819ea644c9ecaf36468ed938461a881
Marciano C. Preciado <marcianocpreciado@gmail.com>
2026-06-02 23:18:43 -0600

    feat: implementing LQR for MAV

--- mavsim_python/controllers/autopilot_lqr.py
+++ mavsim_python/controllers/autopilot_lqr.py
@@ -35,31 +35,31 @@ class Autopilot:
         self.errorAirspeedD1 = 0
         # compute LQR gains
         
-        #### TODO ######
-        CrLat = array([[0, 0, 0, 0, 0]])
+        CrLat = array([[0, 0, 0, 0, 1]])
         AAlat = concatenate((
                     concatenate((M.A_lat, zeros((5,1))), axis=1),
                     concatenate((CrLat, zeros((1,1))), axis=1)),
                     axis=0)
         BBlat = concatenate((M.B_lat, zeros((1,2))), axis=0)
-        Qlat = diag([0, 0, 0, 0, 0, 0]) # v, p, r, phi, chi, intChi
-        Rlat = diag([0, 0]) # a, r
-        # Plat = solve_continuous_are(AAlat, BBlat, Qlat, Rlat)
+        Qlat = diag ([.001 , .01 , .1 , 100, 1, 100]) # v, p, r , phi , chi , intChi
+        Rlat = diag ([1 , 1]) # a, r
+        Plat = solve_continuous_are(AAlat, BBlat, Qlat, Rlat)
         Plat = Plon = np.zeros((6,6))
-        # self.Klat = inv(Rlat) @ BBlat.T @ Plat
-        self.Klat = np.zeros((2,6))
-        CrLon = array([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])
+        self.Klat = inv(Rlat) @ BBlat.T @ Plat
+        
+        u_trim = M.x_trim[3] # state variable u
+        w_trim = M.x_trim[4] # state variable w
+        Va = AP.Va0
+        CrLon = array([[0, 0, 0, 0, 1], [u_trim/Va, w_trim/Va, 0, 0, 0]])
         AAlon = concatenate((
                     concatenate((M.A_lon, zeros((5,2))), axis=1),
                     concatenate((CrLon, zeros((2,2))), axis=1)),
                     axis=0)
         BBlon = concatenate((M.B_lon, zeros((2, 2))), axis=0)
-        Qlon = diag([0, 0, 0, 0, 0, 0, 0]) # u, w, q, theta, h, intH, intVa
-        Rlon = diag([0, 0])  # e, t
-        # Plon = solve_continuous_are(AAlon, BBlon, Qlon, Rlon)
-        Plon = np.zeros((7,7))
-        # self.Klon = inv(Rlon) @ BBlon.T @ Plon
-        self.Klat = np.zeros((2,7))
+        Qlon = diag ([10 , 10 , .001 , .01 , 10 , 100 , 100]) # u, w, q , theta , h, intH , intVa
+        Rlon = diag ([1 , 1]) # e , t
+        Plon = solve_continuous_are(AAlon, BBlon, Qlon, Rlon)
+        self.Klon = inv(Rlon) @ BBlon.T @ Plon
         self.commanded_state = MsgState()
 
     def update(self, cmd, state):
