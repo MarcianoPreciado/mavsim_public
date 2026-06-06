00c43b13db1c53dbacb74dfdef81ae278fa544bd
Marciano C. Preciado <marcianocpreciado@gmail.com>
2026-06-05 23:34:49 -0600

    feat: useful variables for controller parameters

--- mavsim_python/parameters/control_parameters.py
+++ mavsim_python/parameters/control_parameters.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import sqrt
 import models.model_coef as TF
 import parameters.aerosonde_parameters as MAV
 
@@ -8,6 +9,61 @@ gravity = MAV.gravity  # gravity constant
 Va0 = TF.Va_trim
 rho = 1.293 # density of air
 sigma = 0  # low pass filter gain for derivative
+Vg = TF.Va_trim  # ground speed (m/s)
+m = TF.mass
+S = TF.S_wing
+b = TF.b
+c = TF.c
+
+x_star = TF.x_trim
+inputs_star = TF.u_trim
+
+u_star = x_star.item(3)
+v_star = x_star.item(4)
+w_star = x_star.item(6)
+
+phi_star = x_star.item(7)
+theta_star = x_star.item(8)
+psi_star = x_star.item(9)
+
+p_star = x_star.item(9)
+q_star = x_star.item(10)
+r_star = x_star.item(11)
+
+delta_e_star = inputs_star.item(0)
+delta_a_star = inputs_star.item(1)
+delta_r_star = inputs_star.item(2)
+delta_t_star = inputs_star.item(3)
+
+beta_star = psi_star
+
+
+CYdr = MAV.C_Y_delta_r
+CYp = MAV.C_Y_p
+CYr = MAV.C_Y_r
+CYb = MAV.C_Y_beta
+CY0 = MAV.C_Y_0
+CYda = MAV.C_Y_delta_a
+CYdt = MAV.C_Y_delta_t
+Cr0 = MAV.C_n_0
+Crp = MAV.C_n_p
+Crr = MAV.C_n_r
+Crb = MAV.C_n_beta
+Crda = MAV.C_n_delta_a
+Crdr = MAV.C_n_delta_r
+gamma1 = MAV.gamma1
+
+# Lateral state-space model coeﬃcients from Chapter 5 table 5.1
+Yv = rho*S*b*v_star/Va0 * (CYp*p_star + CYr*r_star) + \
+    rho*S*c*CYb/(2*m) * sqrt(u_star**2 + v_star**2) + \
+    rho*S*v_star/m*(CY0 + CYb*beta_star + CYda*delta_a_star + CYdr*delta_r_star)
+Yr = - u_star + rho*S*b*Va0/(4*m)*CYp
+Nv = rho*S*b**2*v_star/(4*Va0) * (Crp * p_star + Crr * r_star) + \
+    rho*S*b*Crb/2 * sqrt(u_star**2 + w_star**2) + \
+    rho*S*b*v_star * (Cr0 + Crb*beta_star + Crda*delta_a_star + Crdr*delta_r_star)
+Nr = -gamma1 * q_star + rho*S*b**2*Va0/4 * Crr
+
+Wx = 10 # bandwidth scale between loops
 
 #----------roll loop-------------
 # get transfer function data for delta_a to phi
