49555c2258a68646fd6e6b9cad92c69083a398e9
Marciano C. Preciado <marcianocpreciado@gmail.com>
2026-06-09 01:13:57 -0600

    feat: first LQR TECs controller with semi-decent performance

--- mavsim_python/controllers/autopilot_lqr_tecs.py
+++ mavsim_python/controllers/autopilot_lqr_tecs.py
@@ -28,11 +28,15 @@ class Autopilot:
         self.Ts = ts_control
         # initialize integrators and delay variables
         self.integratorCourse = 0
+        self.integratorAltitude = 0
+        self.integratorAirspeed = 0
         self.integratorE = 0
         self.integratorL = 0
         self.errorCourseD1 = 0
         self.errorED1 = 0
         self.errorLD1 = 0
+        self.errorAltitudeD1 = 0
+        self.errorAirspeedD1 = 0
         # compute LQR gains
         
         CrLat = array([[0, 0, 0, 0, 1]])
@@ -47,44 +51,35 @@ class Autopilot:
         Plat = solve_continuous_are(AAlat, BBlat, Qlat, Rlat)
         self.Klat = inv(Rlat) @ BBlat.T @ Plat
         
-        AAlon = array([
-            [-0.2822,  0.4946, -1.242,   0.,      0.,      0.,      0.,    0.,    0.    ],
-            [-0.5611, -4.4978, 24.9691,  0.1025,  0.,      0.,      0.,    0.,    0.    ],
-            [ 0.1986, -3.993,   0.,      0.,      0.,      0.,      0.,    0.,    0.    ],
-            [ 0.,      0.,      1.0001,  0.,      0.,      0.,      0.,    0.,    0.    ],
-            [ 0.0497, -0.9988,  0.,     25.,      0.,      0.,      0.,    0.,    0.    ],
-            [10.1811,  0.5064,  0.,      0.9988,  0.,      0.,      0.,    0.,    0.    ],
-            [10.1811,  0.5064,  0.,     -0.9988,  0.,      0.,      0.,    0.,    0.    ]
+        AAlon = np.array([
+            [-0.2822,  0.4946, -1.2123, -9.7979,  0.,      0.,      0.,      0.,      0.    ],
+            [-0.5611, -4.4978, 24.3714, -0.4874,  0.,      0.,      0.,      0.,      0.    ],
+            [ 0.1986, -3.993,  -5.2947,  0.,      0.,      0.,      0.,      0.,      0.    ],
+            [ 0.,      0.,      1.0001,  0.,      0.,      0.,      0.,      0.,      0.    ],
+            [ 0.0497, -0.9988,  0.,     25.,      0.,      0.,      0.,      0.,      0.    ],
+            [-0.0316,  0.0276, -0.,     -0.0012,  0.,      0.,      0.,      0.,      0.    ],
+            [-0.0316,  0.0276, -0.,     -1.9988,  0.,      0.,      0.,      0.,      0.    ],
+            [ 0.,      0.,      0.,      0.,      1.,      0.,      0.,      0.,      0.    ],
+            [ 0.9988,  0.0497,  0.,      0.,      0.,      0.,      0.,      0.,      0.    ],
         ])
-
-        BBlon = array([
-            [-0.1392,   9.4813],
-            [-2.5861,   0.    ],
+        BBlon = np.array([
+            [ -0.1392,  9.4813],
+            [ -2.5861,  0.    ],
             [-36.1124,  0.    ],
-            [ 0.,        0.    ],
-            [ 0.,        0.    ],
-            [ 0.,        0.    ],
-            [ 0.,        0.    ]
+            [  0.,       0.    ],
+            [  0.,       0.    ],
+            [ -0.0273,  0.9653],
+            [ -0.0273,  0.9653],
+            [  0.,       0.    ],
+            [  0.,       0.    ],
         ])
 
-        CClon = array([
-            [10.1811,  0.5064,  0.,      0.9988,  0.,      0.,      0.,    0.,    0.    ],
-            [10.1811,  0.5064,  0.,     -0.9988,  0.,      0.,      0.,    0.,    0.    ],
-            [ 0.,      0.,      0.,      0.,      0.,      1.,      0.,    0.,    0.    ],
-            [ 0.,      0.,      0.,      0.,      0.,      0.,      1.,    0.,    0.    ]
-        ])
-
-        DDlon = array([
-            [0., 0.],
-            [0., 0.],
-            [0., 0.],
-            [0., 0.]
-        ])
-
-        Qlon = diag([10.0, 100.0, 100, 10, 50.0, 100, 100, 10, 10]) # u, w, q, theta, h, Edot, Ldot, Edot_int, Ldot_int
+        Qlon = diag([10.0, 10.0, 100, 50, 50.0, 20, 20, 40., 100.]) # u, w, q, theta, h, Edot_int, Ldot_int, h_int, Va_int
         Rlon = diag ([1 , 1]) # e , t
         Plon = solve_continuous_are(AAlon, BBlon, Qlon, Rlon)
         self.Klon = inv(Rlon) @ BBlon.T @ Plon
+        # C_lon: maps x_lon=[u,w,q,theta,h] -> [Edot, Ldot]; embedded in AAlon rows 5-6
+        self.C_lon = AAlon[5:7, 0:5]
         self.commanded_state = MsgState()
 
     def update(self, cmd, state):
@@ -116,28 +111,44 @@ class Autopilot:
         # longitudinal autopilot
         hr = saturate(hr, state.altitude - 0.2*AP.altitude_zone, state.altitude + 0.2*AP.altitude_zone)
         eh = state.altitude - hr
-        self.integratorE += (eh + self.errorED1) * self.Ts / 2
-        self.errorED1 = eh
-        # if abs(eh) < 0.1*AP.altitude_zone:
-        #     self.integratorE = 0
-        #     self.errorED1 = 0
+        self.integratorAltitude += (eh + self.errorAltitudeD1) * self.Ts / 2
+        self.errorAltitudeD1 = eh
+        if abs(eh) < 0.1*AP.altitude_zone:
+            self.integratorAltitude = 0
+            self.errorAltitudeD1 = 0
+        
+        self.integratorAirspeed += (eVa + self.errorAirspeedD1) * self.Ts / 2
+        self.errorAirspeedD1 = eVa
+        if abs(eVa) < 0.12:
+            self.integratorAirspeed = 0
+            self.errorAirspeedD1 = 0
 
-        self.integratorL += (eVa + self.errorLD1) * self.Ts / 2
-        self.errorLD1 = eVa
-        # if abs(eVa) < 0.12:
-        #     self.integratorL = 0
-        #     self.errorLD1 = 0
-        Edot = 0
-        Ldot = 0
+        
+        x_lon_phys = array([[eVa * cos(state.alpha)],   # eu
+                            [eVa * sin(state.alpha)],   # ew
+                            [state.q],
+                            [state.theta],
+                            [eh]])
+        tecs = self.C_lon @ x_lon_phys
+        Edot = tecs.item(0)
+        Ldot = tecs.item(1)
+
+        self.integratorE += (Edot + self.errorED1) * self.Ts / 2
+        self.errorED1 = Edot
+
+        self.integratorL += (Ldot + self.errorLD1) * self.Ts / 2
+        self.errorLD1 = Ldot
+
         xLon = array([[ eVa * cos(state.alpha) ] , # u
                       [ eVa * sin(state.alpha) ] , # w
                       [ state.q] ,
                       [ state.theta ] ,
                       [ eh ] ,
-                      [ Edot ],
-                      [ Ldot ],
                       [ self.integratorE ] ,
-                      [ self.integratorL ]])
+                      [ self.integratorL ],
+                      [ self.integratorAltitude ] ,
+                      [ self.integratorAirspeed ]])
+        
         tmp = -self.Klon @ xLon
         delta_e = saturate(tmp.item(0), -radians(30), radians(30))
         delta_t = saturate(tmp.item(1), 0, 1)
