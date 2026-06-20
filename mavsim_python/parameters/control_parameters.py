aeb596a24faef63f49dec22498a31d365e813e7b
Marciano C. Preciado <marcianocpreciado@gmail.com>
2026-06-20 11:19:01 -0600

    fix: use parameterized air density

--- mavsim_python/parameters/control_parameters.py
+++ mavsim_python/parameters/control_parameters.py
@@ -7,8 +7,7 @@ import parameters.aerosonde_parameters as MAV
 #### TODO #####
 gravity = MAV.gravity  # gravity constant
 Va0 = TF.Va_trim
-rho = 1.293 # density of air
-sigma = 0  # low pass filter gain for derivative
+rho = MAV.rho # density of air
 Vg = TF.Va_trim  # ground speed (m/s)
 m = MAV.mass
 S = MAV.S_wing
